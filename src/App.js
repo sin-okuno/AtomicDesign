6811aa49b1a768765b05717eebf3a980b970e6ae
sin-okuno <73112448+sin-okuno@users.noreply.github.com>
2021-06-26 06:49:54 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import { SearchInput } from "./components/molecules/SearchInput";
+
+export default function App() {
+  return (
+    <div className="App">
+      <SearchInput />
+    </div>
+  );
+}
---
+++ src/components/molecules/SearchInput.jsx
@@ -0,0 +1,10 @@
+import { PrimaryButton } from "../atoms/buttoms/PrimaryButton";
+
+export const SearchInput = () => {
+  return (
+    <div>
+      <input type="text" />
+      <PrimaryButton>検索</PrimaryButton>
+    </div>
+  );
+};
